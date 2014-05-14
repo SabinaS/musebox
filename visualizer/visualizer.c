a171ea0d4a2caae14c63b52da2019ce60eaf97de
SabinaS <SabinaS@users.noreply.github.com>
2014-05-14 12:42:26 -0400

    Update visualizer.c

--- visualizer/visualizer.c
+++ visualizer/visualizer.c
@@ -9,32 +9,29 @@
 #include <unistd.h>
 //#include "usbkeyboard.h"
 #include <math.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "visualizer_driver.h"
 
 #define USER_SPACE_FFT
-typedef struct {
-    int16_t real;
-    int16_t imag;
-} complex_num;
-typedef struct {
-    int addr;
-    int height; 
-} freq_slot;
+
 
 #define SAMPLENUM 8192
 #define H25K 4096
 static int slot_values[12] = {31, 72, 150, 250, 440, 630, 1000, 2500, 5000, 8000, 14000, 20000};
 static int bin_centers[12] = {6, 13, 28, 46, 82, 117, 186, 464, 929, 1486, 2601, 3715};
+static int slot_heights[12]; 
+static struct freq_slot slot_amps[12]; 
 static struct complex_num freq_data[SAMPLENUM];
 
 void read_samples()
 {
     int fd = fopen("/dev/freq_spec", O_RDWR); //file descriptor? 
-    struct freq_bin *freq_data;
-    freq_data = (freq_bin*) malloc(SAMPLEBYTES); //allocating space for data array 
+    struct complex_num *freq_data;
+    freq_data = (complex_num*) malloc(SAMPLEBYTES); //allocating space for data array 
     
     if (ioctl(fd, VISUALIZER_DRIVER_READ_FFT, freq_data) == -1) //automatically updates freq_data? 
         printf("VISUALIZER_DRIVER_READ_FFT failed: %s\n",
