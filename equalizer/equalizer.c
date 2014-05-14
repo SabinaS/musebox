8e785a37d0af55cc6e9d38409db151bf0d3d7177
SabinaS <SabinaS@users.noreply.github.com>
2014-05-14 10:39:12 -0400

    Update equalizer.c

--- equalizer/equalizer.c
+++ equalizer/equalizer.c
@@ -21,7 +21,7 @@
 
 int fp; 
 
-void write_db(uint8_t* db_value, send_info* send)
+void write_db(send_info* send)
 {
     int fd = open("/dev/freq_spec", O_RDWR);    
     if (ioctl(fd, EQUALIZER_DRIVER_WRITE_DIGIT, send) == -1)
@@ -38,20 +38,20 @@ void write_db(uint8_t* db_value, send_info* send)
 int main()
 {
     int freq,db;
-    printf("Please type in the frequency you want to change (1 to 12):\n");
+    printf("Please type in the frequency you want to change (0 to 11):\n");
     scanf("%d",&freq);
-    printf("Please type in the decibal value you would like to change it to (-12 to 12):\n");
+    printf("Please type in the decibal value you would like to change it to (1 to 25):\n");
     scanf("%d",&db);
 
-    uint8_t addr, db_value; 
-    addr = (uint8_t *) ("4'd" + freq - 1); 
-    db_value = (uint8_t *) db; 
+    int addr, db_value; 
+    addr = (int) freq; 
+    db_value = (int) db; 
     
     struct send_info sendi;
     sendi.addr = addr; 
     sendi.db = db_value; 
 
-    write_db(db_value, sendi); 
+    write_db(sendi); 
     
     printf("Equalizer Userspace program terminating\n");
     return 0;
