7ea0bc815663708c91d7d52d7e6f16a19c3946ab
kugurst <ma2799@gmail.com>
2014-05-14 17:55:56 -0400

    Added a simple audio tester.

---
+++ visualizer/audio-test.c
@@ -0,0 +1,58 @@
+/* 
+ * Creates frequency spectrum visualizer for different audio frequencies
+ */ 
+
+//#include "fbputchar.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+//#include "usbkeyboard.h"
+#include <math.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define CPU_AUDIO_US
+#include "cpu_audio.h"
+
+#define SAMPLENUM 8192
+#define H25K 4096
+
+int main()
+{
+    char *file = "/dev/musebox";
+    int box_fd;
+
+    if ((box_fd = open(file, O_RDWR)) == -1 ) {
+        fprintf(stderr, "could not open %s\n", file);
+        return -1;
+    }
+
+    // int bar = 0;
+    // int height = 240;
+    // int dir = 0;
+    // while (bar < 12) {
+    //     if (dir == 0)
+    //         height--;
+    //     else
+    //         height++;
+    //     slot.addr = bar++;
+    //     if (bar == 12)
+    //         bar = 0;
+    //     slot.height = height;
+    //     if (ioctl(frec_spec_fd, VISUALIZER_WRITE_FREQ, &slot)) {
+    //         perror("ioctl write failed!");
+    //         close(frec_spec_fd);
+    //         return -1;
+    //     }
+    //     usleep(1/60.0 * 100000);
+    //     if (height == 479)
+    //         dir = 0;
+    //     else if (height == 0)
+    //         dir = 1;
+    // }
+    close(box_fd);
+    return 0;
+}
