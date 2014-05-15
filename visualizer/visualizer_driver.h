8f17aea50365c00a9a5cd9c00efc532a3ad25e8c
SabinaS <SabinaS@users.noreply.github.com>
2014-05-14 21:16:49 -0400

    Update visualizer_driver.h

--- visualizer/visualizer_driver.h
+++ visualizer/visualizer_driver.h
@@ -11,11 +11,41 @@ typedef struct {
 } complex_num;
 typedef struct {
     int addr;
-    int height; 
+    int height;
 } freq_slot;
 
 /* ioctls and their arguments */
-#define VISUALIZER_DRIVER_WRITE_FREQ _IOW(VISUALIZER_MAGIC, 1, u32 *) //writes to freq_spec.sv 
-#define VISUALIZER_DRIVER_READ_FFT _IOW(VISUALIZER_MAGIC, 1, u32 *) //writes to freq_spec.sv 
+#define VISUALIZER_DRIVER_WRITE_FREQ _IOW(VISUALIZER_MAGIC, 1, u32 *)  
+#define VISUALIZER_DRIVER_READ_FFT _IOW(VISUALIZER_MAGIC, 1, u32 *) 
 
 #endif
+
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+~                                                                                                                       
+"visualizer_driver.h" 22L, 429C                                                                       18,1          All
