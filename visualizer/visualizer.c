77aa4d787d1f661973182ba9ba9ea32cdf45bce1
SabinaS <SabinaS@users.noreply.github.com>
2014-05-14 21:16:13 -0400

    Update visualizer.c

--- visualizer/visualizer.c
+++ visualizer/visualizer.c
@@ -1,6 +1,5 @@
-/* 
- * Creates frequency spectrum visualizer for different audio frequencies
- */ 
+/*creates frequency spectrum visualizer for different audio frequencies
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,26 +23,17 @@
 
 static int slot_values[12] = {31, 72, 150, 250, 440, 630, 1000, 2500, 5000, 8000, 14000, 20000};
 static int bin_centers[12] = {6, 13, 28, 46, 82, 117, 186, 464, 929, 1486, 2601, 3715};
-static int slot_heights[12]; 
-static struct freq_slot slot_amps[12]; 
+static int slot_heights[12];
+static struct freq_slot slot_amps[12];
 static struct complex_num freq_data[SAMPLENUM];
-
-typedef struct {
-    int16_t real;
-    int16_t imag;
-} complex_num;
-typedef struct {
-    int addr;
-    int height; 
-} freq_slot;
 
 void read_samples()
 {
-    int fd = fopen("/dev/freq_spec", O_RDWR); 
+    int fd = fopen("/dev/freq_spec", O_RDWR);
     struct complex_num *freq_data;
-    freq_data = (complex_num*) malloc(SAMPLENUM*2); 
-    
-    if (ioctl(fd, VISUALIZER_DRIVER_READ_FFT, freq_data) == -1) 
+    freq_data = (complex_num*) malloc(SAMPLENUM*2);
+
+    if (ioctl(fd, VISUALIZER_DRIVER_READ_FFT, freq_data) == -1)
         printf("VISUALIZER_DRIVER_READ_FFT failed: %s\n",
             strerror(errno));
     else {
@@ -52,12 +42,12 @@ void read_samples()
         else
             puts("VISUALIZER_DRIVER_READ_FFT is set");
     }
-    
+
 }
 
 void write_samples(int* dataArray)
 {
-    int fd = open("/dev/freq_spec", O_RDWR);    
+    int fd = open("/dev/freq_spec", O_RDWR);
     if (ioctl(fd, VISUALIZER_DRIVER_WRITE_FREQ, dataArray) == -1){
         printf("VISUALIZER_DRIVER_WRITE_FREQ failed: %s\n", strerror(errno));
     }
@@ -71,46 +61,44 @@ void write_samples(int* dataArray)
     }
 }
 
-
 int main()
 {
-    read_samples(); 
-    int i = 0; 
-    int j = 0; 
-    
-   
-    while(i= 1; i< 13; i++){
-	    double ampl_real = 0; 
-	    double ampl_imag = 0; 
-		while(j=slot_values[i-1]; j< slot_values[i+1]; j++){
-		    if(j<slot_values[i]){
-		        ampl_real = ampl_real + (freq_data[i].real) * ((freq_data[j].real - slot_values[i-1])/(slot_values[i]- slot_values[i-1])); 
-		        ampl_imag = ampl_imag + (freq_data[i].imag) * ((freq_data[j].imag - slot_values[i-1])/(slot_values[i]- slot_values[i-1]));
-		    }
-		    else if(j>slot_values[i]){
-		        ampl_real = ampl_real + (freq_data[i].real) * ((slot_values[i+1]-slot_values[i])/(slot_values[i+1]-freq_data[j].real)); 
-		        ampl_imag = ampl_imag + (freq_data[i].imag) * ((slot_values[i+1]-slot_values[i])/(slot_values[i+1]-freq_data[j].imag)); 
-		    }
-		    else{
-		        ampl_real = ampl_real + freq_data[j].real; 
-		        ampl_imag = ampl_imag + freq_data[j].imag; 
-		    }
-		}
-		
-	    double amp = (20*(log10((ampl_real)*(ampl_real) + (ampl_imag)*(ampl_imag)))); 
-	    slot_amps[i] = amp;
+    read_samples();
+    int i, j;
+
+    for(i= 1; i< 13; i++){
+            double ampl_real = 0;
+            double ampl_imag = 0;
+                for(j=slot_values[i-1]; j< slot_values[i+1]; j++){
+                    if(j<slot_values[i]){
+                        ampl_real = ampl_real + (freq_data[i].real) * ((freq_data[j].real - slot_values[i-1])/(slot_values[i]- slot_values[i-1]));
+                        ampl_imag = ampl_imag + (freq_data[i].imag) * ((freq_data[j].imag - slot_values[i-1])/(slot_values[i]- slot_values[i-1]));
+                    }
+                    else if(j>slot_values[i]){
+                        ampl_real = ampl_real + (freq_data[i].real) * ((slot_values[i+1]-slot_values[i])/(slot_values[i+1]-freq_data[j].real));
+                        ampl_imag = ampl_imag + (freq_data[i].imag) * ((slot_values[i+1]-slot_values[i])/(slot_values[i+1]-freq_data[j].imag));
+                    }
+                    else{
+                        ampl_real = ampl_real + freq_data[j].real;
+                        ampl_imag = ampl_imag + freq_data[j].imag;
+                    }
+                }
+
+            double amp = (20*(log10((ampl_real)*(ampl_real) + (ampl_imag)*(ampl_imag))));
+            slot_amps[i] = amp;
     }
-    
-    
-    while(i=1; i < 13; i++){
-	int height;
-	height = 479 - ((slot_amps[i])/186)*479; 
-	slot_heights[i] = height; 
+
+
+    for( i = 1; i < 13; i++){
+        int height;
+        height = 479 - ((slot_amps[i])/186)*479;
+        slot_heights[i] = height;
 
     }
-    
-    write_samples(slot_heights); 
-    
+
+    write_samples(slot_heights);
+
     printf("Visualizer Userspace program terminating\n");
     return 0;
 }
+
