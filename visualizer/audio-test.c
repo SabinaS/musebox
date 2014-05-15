0ccaf5fbbb00d283bac01954ac6637e20bc4ba47
kugurst <ma2799@gmail.com>
2014-05-15 12:21:40 -0400

    Writing garbage audio in audio-test.

--- visualizer/audio-test.c
+++ visualizer/audio-test.c
@@ -40,8 +40,13 @@ int main()
     //     if (bar == 12)
     //         bar = 0;
     //     slot.height = height;
-    if (ioctl(box_fd, CPU_AUDIO_READ_SAMPLES, samples)) {
+    if (ioctl(box_fd, CPU_AUDIO_WRITE_SAMPLES, samples)) {
         perror("ioctl write failed!");
+        close(box_fd);
+        return -1;
+    }
+    if (ioctl(box_fd, CPU_AUDIO_READ_SAMPLES, samples)) {
+        perror("ioctl read failed!");
         close(box_fd);
         return -1;
     }
