bf598acf3ab1b5f2500195a7f21242065c40627d
kugurst <ma2799@gmail.com>
2014-05-15 09:52:10 -0400

    Golden visualizer_driver

--- visualizer/visualizer_driver.h
+++ visualizer/visualizer_driver.h
@@ -2,63 +2,15 @@
 #define _VISUALIZER_DRIVER_H
 
 #include <linux/ioctl.h>
-#include "visualizer.h"
 
-#define VGA_LED_MAGIC 'q'
+#define VISUALIZER_MAGIC 4112
 
-<<<<<<< HEAD
-struct freq_bin{
-    int real;
-    int imag;  
-    double scaled_ampl; 
-};
-
-/* ioctls and their arguments */
-#define VISUALIZER_WRITE_FREQ _IOW(VISUALIZER_MAGIC, 1, u32 *) //writes to freq_spec.sv 
-#define VISUALIZER_READ_FFT  _IOWR(VISUALIZER_MAGIC, 2, u32 *) //reads from aud_to_fft
-=======
-typedef struct {
-    int16_t real;
-    int16_t imag;
-} complex_num;
 typedef struct {
     int addr;
-    int height;
+    int height; 
 } freq_slot;
 
 /* ioctls and their arguments */
-#define VISUALIZER_DRIVER_WRITE_FREQ _IOW(VISUALIZER_MAGIC, 1, u32 *)  
-#define VISUALIZER_DRIVER_READ_FFT _IOW(VISUALIZER_MAGIC, 1, u32 *) 
->>>>>>> 8f17aea50365c00a9a5cd9c00efc532a3ad25e8c
+#define VISUALIZER_WRITE_FREQ _IOW(VISUALIZER_MAGIC, 1, freq_slot *) //writes to freq_spec.sv 
 
 #endif
-
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-~                                                                                                                       
-"visualizer_driver.h" 22L, 429C                                                                       18,1          All
