580c1d7bb76111a0109cc0cf4032a15a1a852245
kugurst <ma2799@gmail.com>
2014-05-15 15:00:10 -0400

    Fixed struct stack initialization.

--- visualizer/audio-test.c
+++ visualizer/audio-test.c
@@ -27,7 +27,7 @@ int main()
     char *file = "/dev/cpu_audio";
     int box_fd;
 
-    struct sample samples[SAMPLENUM] = {0};
+    struct sample samples[SAMPLENUM] = {{0,0}};
     int i;
     // 1 K sine wave
     for (i = 0; i < SAMPLENUM; i++) {
